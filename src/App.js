61b4fb55ef7f56305f5ab805c601b4c735bbf1df
Vadim <vadik.kortkov@yandex.ru>
2022-06-18 11:23:29 +0300

    some apdate

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Card from './components/Card/Card';
 
 function App() {
   const cards = [
-    {id:1,title:'Сказочное заморское яство',name:'Нямушка',subtitle:'с фуа-гра',amount:'10 порций',dop1:'мышь в подарок',weight:'0,5',weight_dop:'кг',first:true,description:'Чего сидишь? Порадуй котэ, ',description_dop:'купи',selected:false},
-    {id:2,title:'Сказочное заморское яство',name:'Нямушка',subtitle:'с рыбой',amount:'40 порций',dop1:'2 мыши в подарок',weight:'2',weight_dop:'кг',description:'Печень утки разварная с артишоками.',selected:true},
-    {id:3,title:'Сказочное заморское яство',name:'Нямушка',subtitle:'с курой',amount:'100 порций',dop1:'5 мышей в подарок',dop2:'заказчик доволен',weight:'5',weight_dop:'кг',description:'Печалька, с курой закончился.',disabled:true},
+    {id:1,title:'Сказочное заморское яство',title_hover:'Котэ не одобряет?',name:'Нямушка',subtitle:'с фуа-гра',amount:'10 порций',dop1:'мышь в подарок',weight:'0,5',weight_dop:'кг',first:true,description:'Чего сидишь? Порадуй котэ, ',description_dop:'купи',selected:false,description_selected:'Печень утки разварная с артишоками.'},
+    {id:2,title:'Сказочное заморское яство',title_hover:'Котэ не одобряет?',name:'Нямушка',subtitle:'с рыбой',amount:'40 порций',dop1:'2 мыши в подарок',weight:'2',weight_dop:'кг',description:'Чего сидишь? Порадуй котэ, ',description_dop:'купи',description_selected:'Головы щучьи с чесноком да свежайшая сёмгушка.',selected:true},
+    {id:3,title:'Сказочное заморское яство',title_hover:'Котэ не одобряет?',name:'Нямушка',subtitle:'с курой',amount:'100 порций',dop1:'5 мышей в подарок',dop2:'заказчик доволен',weight:'5',weight_dop:'кг',description:'Печалька, с курой закончился.',disabled:true},
   ]
   return (
     <div className="App">
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './Card.css';
 export default function Card(props) {
     const [selected, setSelected] = useState(props.item.selected)
+    const [hover, setHover] = useState(false)
     const [disabled] = useState(props.item.disabled)
     function handleClick() {
         setSelected(!selected)
@@ -15,11 +16,17 @@ export default function Card(props) {
             return "card__content"
         }
     }
+    function handleHover(e) {
+        if (e.target.getAttribute('data-modal') === 'true') {
+            setHover(true)
+        }
+    }
+
     return (
         <div className="card" >
-            <div className={statusCheck()} onClick={handleClick}>
+            <div className={statusCheck()} onClick={handleClick} data-modal={selected} onMouseEnter={(e) => handleHover(e)} onMouseLeave={() => setHover(false)}>
                 <div className="card__info">
-                    <p className="card__title">{props.item.title}</p>
+                    <p className={hover ? "card__title card__title_hover" : "card__title"}>{hover ? `${props.item.title_hover}` : `${props.item.title}`}</p>
                     <h1 className="card__name">{props.item.name}</h1>
                     <h4 className="card__subtitle">{props.item.subtitle}</h4>
                     <p>{props.item.amount}</p>
@@ -28,7 +35,10 @@ export default function Card(props) {
                 </div>
                 <h2 className={props.item.first ? "card__weight card__weight_first" : "card__weight"}>{props.item.weight}<span>{props.item.weight_dop}</span></h2>
             </div>
-            <span className="card__description">{props.item.description}<span className="card__description_dop" onClick={handleClick}>{props.item.description_dop}</span></span>
+            {selected
+                ? <span className="card__description">{props.item.description_selected}</span>
+                : <span className={disabled ? "card__description card__description_disabled" : "card__description"}>{props.item.description}<span className="card__description_dop" onClick={handleClick}>{props.item.description_dop}</span></span>
+            }
         </div>
     )
 }
