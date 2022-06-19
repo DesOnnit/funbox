705f8e743250d62b7b3c31d6a03b2c8269186281
Vadim <vadik.kortkov@yandex.ru>
2022-06-19 09:09:50 +0300

    bug fixed

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,6 +6,7 @@ export default function Card(props) {
     const [disabled] = useState(props.item.disabled)
     function handleClick() {
         setSelected(!selected)
+        setHover(false)
     }
     function statusCheck() {
         if (selected) {
